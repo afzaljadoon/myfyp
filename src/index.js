800f514dfb232c1cdcf2297e7602554d550f5a2d
Afzal Khan Jadoon <jadoonafzal027@gmail.com>
2024-09-03 20:18:59 +0500

    changes

--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { CandidatesProvider } from './components/CandidatesContext';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
